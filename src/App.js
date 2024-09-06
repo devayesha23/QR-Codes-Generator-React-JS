0b915ff61560c9cd6d725c11807d7b30cef4ce56
devayesha23 <zahraayesha414@gmail.com>
2024-09-06 13:06:51 +0500

    QR Codes Generator App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useState } from "react";
+import { init } from 'filestack-js';
+import "./App.css"; // Import the styles
+
+const client = init("A18L3T2eWRemxYIGwGlZsz");
+
 
 function App() {
+  const [qrCodeUrl, setQrCodeUrl] = useState(null);
+
+  const handleUpload = () => {
+    client
+      .picker({
+        accept: "image/*",
+        onUploadDone: (res) => {
+          const handle = res.filesUploaded[0].handle;
+          generateQRCode(handle);
+        },
+      })
+      .open();
+  };
+
+  const generateQRCode = (handle) => {
+    const qrUrl = `https://cdn.filestackcontent.com/apikey/qr/${handle}`;
+    setQrCodeUrl(qrUrl);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="container">
+      <img
+        src="https://img.freepik.com/premium-vector/qr-code-cartoon-character-searching-with-magnifying-glass-cute-design_152558-13614.jpg"
+        alt="Cartoon Banner"
+        className="banner-img"
+      />
+      <h1>Filestack QR Code Generator</h1>
+      <button id="uploadBtn" onClick={handleUpload}>
+        Upload File
+      </button>
+
+      <div id="qrCode">
+        {qrCodeUrl && (
+          <img src={qrCodeUrl} alt="QR Code" style={{ maxWidth: "100%", height: "auto" }} />
+        )}
+      </div>
+
+      {qrCodeUrl && (
+        <a id="downloadLink" href={qrCodeUrl} download="qrcode.png">
+          <button id="downloadBtn">Download QR Code</button>
         </a>
-      </header>
+      )}
     </div>
   );
 }
